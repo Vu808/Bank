25418361b6be24c941a26b826ed7dea0291fd239
Vu808 <31222309+Vu808@users.noreply.github.com>
2017-08-31 10:45:52 -1000

    Add files via upload

--- SavingsAccount.java
+++ SavingsAccount.java
@@ -1,4 +1,25 @@
 
-public class SavingsAccount {
+public class SavingsAccount extends Account {
 
-}
+	private static double rate = 0.05;
+	private double interest;
+	
+	public SavingsAccount(double money, int accountNumber, double interest ) {
+		super(money, accountNumber);
+	this.interest = interest;
+	}
+	
+	public double getInterest() {
+		return interest;
+		
+	}
+	
+	public void setRate(int Rate) {
+		this.rate = rate;
+	}
+		
+		public void calculateInterest() {
+		interest = getMoney() * rate;	
+		}
+	}
+
